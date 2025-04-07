bfe9766310911d2a4ec4c3659bc3822243f26cd5
Tawanda Andrew Msengezi <xrthemc@gmail.com>
2025-04-07 13:08:14 +0200

    Refactor event fetching functions for improved error handling and update sitemap with new last modified dates

--- app/page.jsx
+++ app/page.jsx
@@ -1,45 +1,29 @@
 import Home from "./(components)/Home";
 
 const fetchAllEvents = async () => {
-  try {
-    const res = await fetch(
-      `${process.env.HREF}/api/AllEvents?limit=${Infinity}`,
-      {
-        cache: "no-store",
-      }
-    );
+  const res = await fetch(`${process.env.HREF}/api/AllEvents?limit=${Infinity}`);
+  const { data } = await res.json();
 
-    const { data } = await res.json();
-
-    return data.map((event) => ({
-      ...event,
-      formattedDate: new Date(event.date).toLocaleDateString("en-US", {
-        month: "short",
-        day: "numeric",
-      }),
-    }));
-  } catch (error) {
-    console.error(error);
-  }
+  return data.map((event) => ({
+    ...event,
+    formattedDate: new Date(event.date).toLocaleDateString("en-US", {
+      month: "short",
+      day: "numeric",
+    }),
+  }));
 };
-// Format dates during data fetching
+
 const fetchSomeEvents = async () => {
-  try {
-    const res = await fetch(`${process.env.HREF}/api/events`, {
-      cache: "no-store",
-    });
-    const data = await res.json();
-    return data.events.map((event) => ({
-      ...event,
-      formattedDate: new Date(event.date).toLocaleDateString("en-US", {
-        day: "2-digit",
-        month: "short",
-        year: "numeric",
-      }),
-    }));
-  } catch (error) {
-    console.error(error);
-  }
+  const res = await fetch(`${process.env.HREF}/api/events`);
+  const data = await res.json();
+  return data.events.map((event) => ({
+    ...event,
+    formattedDate: new Date(event.date).toLocaleDateString("en-US", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    }),
+  }));
 };
 
 export default async function Page() {
